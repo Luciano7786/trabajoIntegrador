bf6c3df7468e1480be4636dcf4a346d10205e1f6
Mariano Jimenez <104471721+Marianoj1991@users.noreply.github.com>
2025-05-28 22:31:15 -0300

    Estilos en menu desplegable corregidos

--- js/header-boton.js
+++ js/header-boton.js
@@ -1,40 +1,44 @@
 import { obtenerUsuarioLocalStorage } from "./utilidades.js";
+document.addEventListener('DOMContentLoaded', () => {
 
-const usuario = obtenerUsuarioLocalStorage()
+  const usuario = obtenerUsuarioLocalStorage()
+  
+  const ulNavbar = document.getElementById('ul-navbar')
+  
+  if (!usuario) {
+  
+    const li = document.createElement('li')
+    li.classList.add('nav-item', 'p-2', 'm-2', 'hover-effect-list-item')
+    const a = document.createElement('a')
+    a.href = 'login.html'
+    a.classList.add('nav-link', 'text-white', 'text-center')
+    a.textContent = 'Iniciar Sesión'
+    li.appendChild(a)
+    ulNavbar.appendChild(li)
+  
+  } else {
+    const liRegistrarSalon = document.createElement('li')
+    liRegistrarSalon.classList.add('nav-item', 'p-2', 'm-2', 'hover-effect-list-item')
+    const aRegistrarSalon = document.createElement('a')
+    aRegistrarSalon.classList.add('nav-link', 'text-white', 'text-center')
+    aRegistrarSalon.textContent = 'Registrar Salón'
+    aRegistrarSalon.href = './altaSalones.html'
+    liRegistrarSalon.appendChild(aRegistrarSalon)
+    ulNavbar.appendChild(liRegistrarSalon)
+    const li = document.createElement('li')
+    li.classList.add('nav-item', 'p-2', 'm-2', 'hover-effect-list-item')
+    const boton = document.createElement('a')
+    boton.textContent = 'Cerrar Sesión'
+    boton.classList.add('nav-link', 'text-white', 'text-center')
+    boton.style.cursor = 'pointer'
+    const liContacto = document.getElementById('li-contacto').remove()
+    
+    boton.addEventListener('click', () => {
+      localStorage.removeItem('usuario')
+      window.location.href = 'login.html'
+    })
+    li.appendChild(boton)
+    ulNavbar.appendChild(li)
+  }
 
-const ulNavbar = document.getElementById('ul-navbar')
-
-if (!usuario) {
-
-  const li = document.createElement('li')
-  li.classList.add('av-item', 'p-2', 'm-2', 'hover-effect-list-item')
-  const a = document.createElement('a')
-  a.href = 'login.html'
-  a.classList.add('nav-link', 'text-white', 'text-center')
-  a.textContent = 'Iniciar Sesión'
-  li.appendChild(a)
-  ulNavbar.appendChild(li)
-
-} else {
-  const liRegistrarSalon = document.createElement('li')
-  liRegistrarSalon.classList.add('av-item', 'p-2', 'm-2'  , 'hover-effect-list-item')
-  const aRegistrarSalon = document.createElement('a')
-  aRegistrarSalon.classList.add('nav-link', 'text-white', 'text-center')
-  aRegistrarSalon.textContent = 'Registrar Salón'
-  aRegistrarSalon.href = './altaSalones.html'
-  liRegistrarSalon.appendChild(aRegistrarSalon)
-  ulNavbar.appendChild(liRegistrarSalon)
-  const li = document.createElement('li')
-  li.classList.add('av-item', 'p-2', 'm-2'  , 'hover-effect-list-item')
-  const boton = document.createElement('button')
-  boton.classList.add('nav-link', 'text-white', 'text-center')
-  boton.textContent = 'Cerrar Sesión'
-  li.appendChild(boton)
-  ulNavbar.appendChild(li)
-  const liContacto = document.getElementById('li-contacto').remove()
-
-  boton.addEventListener('click', () => {
-    localStorage.clear()
-    window.location = 'login.html'
-  })
-}
+})
